b3c70ee5170f46ca9474bfb4082f37be45966118
FeraBinHussain <ferasbinhussain@gmail.com>
2022-01-21 16:22:39 +0300

    adding fav & editing projects page

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,6 @@ class MyFooter extends HTMLElement {
             <div class="row">
                 <div class="col-sm-6">
                     <h3>Contatct Info</h3>
-                    <br>
                     <p>Feras Bin Hussain, Riyadh, Saudi Arabia</p>
                     <a class="fa fa-phone"> +966 545820593</a>
 
@@ -38,8 +37,7 @@ class MyFooter extends HTMLElement {
                 <div class="col-sm-6">
                     <h3>Connect</h3>
                     <a href="https://www.linkedin.com/in/feras-binhussain/" class="fa fa-linkedin"></a>
-                    <a href="mailto:ferasbinhussain@gmail.com" class="fa fa-google"></a>
-                    <a href="mailto:ferasbinhussain@gmail.com" class="fa fa-github"></a>
+                    <a href="https://github.com/FerasBinHussain" class="fa fa-github"></a>
                     <a href="https://twitter.com/Feras11000" class="fa fa-twitter"></a>
                     <a href="https://www.youtube.com/channel/UCljpiADJUwrSs-o4yrvbb0w" class="fa fa-youtube"></a>
                 </div>
